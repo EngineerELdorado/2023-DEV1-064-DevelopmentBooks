95d7c7440f89f5f6c9db44b7b82c809ce9b42433
Denis Kalenga Watshipamba <deniskalenga94@gmail.com>
2023-05-10 22:04:30 +0200

    Revert "refactor(discount) Make DiscountService object reusable in DiscountServiceTest class"
    This reverts commit 7837bbdb9e4de756126d885c2a906abd487facc1.

--- src/test/java/com/bnp/bnp/discount/services/DiscountServiceTest.java
+++ src/test/java/com/bnp/bnp/discount/services/DiscountServiceTest.java
@@ -1,21 +1,16 @@
 package com.bnp.bnp.discount.services;
 
 import com.bnp.bnp.basket.exceptions.EmptyBasketException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DiscountServiceTest {
 
-    DiscountService discountService;
-    @BeforeEach
-    void setUp() {
-        discountService = new DiscountService();
-    }
-
     @Test
     void given_empty_shopping_basket_when_calculating_the_price_then_throw_an_exception() {
+        DiscountService discountService = new DiscountService();
+
         //Given
         int[] shoppingBasket = {};
 
@@ -27,6 +22,8 @@ class DiscountServiceTest {
 
     @Test
     void given_null_shopping_basket_when_calculating_the_price_then_throw_an_exception() {
+        DiscountService discountService = new DiscountService();
+
         //Given //When //Then
         assertThatThrownBy(() -> discountService.calculatePrice(null))
                 .isInstanceOf(EmptyBasketException.class)
