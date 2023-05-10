1fb147a9e98f407de5fa8c6087436af5feade4c1
Denis Kalenga Watshipamba <deniskalenga94@gmail.com>
2023-05-10 22:36:46 +0200

    refactor(discount): Improve codes. Add some spaces in between lines

--- src/main/java/com/bnp/bnp/discount/services/DiscountService.java
+++ src/main/java/com/bnp/bnp/discount/services/DiscountService.java
@@ -13,13 +13,16 @@ public class DiscountService {
 
     public double calculatePrice(@Nullable int[] shoppingBasket) {
         validateShoppingBasket(shoppingBasket);
+
         if (shoppingBasket.length == 1) {
             return PRICE_PER_BOOK;
         }
+
         Set<Integer> distinctBooksMap = new HashSet<>();
         for (int book : shoppingBasket) {
             distinctBooksMap.add(book);
         }
+
         if (distinctBooksMap.size() == 1) {
             return PRICE_PER_BOOK * shoppingBasket.length;
         }
