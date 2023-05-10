284f514f2586efcb837d40538d8229a5560021a4
Denis Kalenga Watshipamba <deniskalenga94@gmail.com>
2023-05-10 22:16:13 +0200

    feat(discount): Apply no discount when only one book is selected

--- src/main/java/com/bnp/bnp/discount/services/DiscountService.java
+++ src/main/java/com/bnp/bnp/discount/services/DiscountService.java
@@ -5,12 +5,18 @@ import jakarta.annotation.Nullable;
 
 public class DiscountService {
 
-    public void calculatePrice(@Nullable int[] shoppingBasket) {
-        if (shoppingBasket==null){
+    private static final double PRICE_PER_BOOK = 50;
+
+    public double calculatePrice(@Nullable int[] shoppingBasket) {
+        if (shoppingBasket == null) {
             throw new EmptyBasketException("The basket is empty");
         }
         if (shoppingBasket.length == 0) {
             throw new EmptyBasketException("The basket is empty");
         }
+        if (shoppingBasket.length == 1) {
+            return PRICE_PER_BOOK;
+        }
+        return 0;
     }
 }
