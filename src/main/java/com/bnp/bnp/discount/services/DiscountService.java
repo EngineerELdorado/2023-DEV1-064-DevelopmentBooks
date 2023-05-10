cd1ee9b73020c7a2a7d9c8bccf0747586e453e53
Denis Kalenga Watshipamba <deniskalenga94@gmail.com>
2023-05-10 21:59:44 +0200

    feat(discount): Throw an exception when the shoppping basket is null

--- src/main/java/com/bnp/bnp/discount/services/DiscountService.java
+++ src/main/java/com/bnp/bnp/discount/services/DiscountService.java
@@ -1,10 +1,14 @@
 package com.bnp.bnp.discount.services;
 
 import com.bnp.bnp.basket.exceptions.EmptyBasketException;
+import jakarta.annotation.Nullable;
 
 public class DiscountService {
 
-    public void calculatePrice(int[] shoppingBasket) {
+    public void calculatePrice(@Nullable int[] shoppingBasket) {
+        if (shoppingBasket==null){
+            throw new EmptyBasketException("The basket is empty");
+        }
         if (shoppingBasket.length == 0) {
             throw new EmptyBasketException("The basket is empty");
         }
